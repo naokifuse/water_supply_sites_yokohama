a200baf5333c829185cbadb841a818717acae300
naokifuse <nao@naoMacbookAir.local>
2015-07-20 14:15:05 +0900

    update hover information

--- server.R
+++ server.R
@@ -3,15 +3,20 @@
 # server.R
 # -----------------------------------------------
 
-shinyServer(function (input, output) {
-  # load data
-  df <- "http://www.city.yokohama.lg.jp/somu/org/kikikanri/data/watersupplysites.xml" %>%
-    xmlToDataFrame(stringsAsFactors=F, 
-                   colClasses=c(rep("character", 4), rep("numeric", 2), "character")) %>% 
-    mutate(id = 1:n(),
-           popup = sprintf("種類: %s <br / > 名称: %s <br /> 住所: %s %s <br /><br /> %s", 
-                           Type, Name, Ward, Address, Notes))
+# load data
+# source: 
+#   http://www.city.yokohama.lg.jp/somu/org/kikikanri/data/watersupplysites.xml
+df <- "watersupplysites.xml" %>%
+  xmlToDataFrame(stringsAsFactors=F, 
+                 colClasses=c(rep("character", 4), rep("numeric", 2), "character")) %>% 
+  mutate(id = 1:n(),
+         popup = sprintf("種類: %s <br / > 名称: %s <br /> 住所: %s %s <br /><br /> %s", 
+                         Type, Name, Ward, Address, Notes),
+         color = ifelse(Type == "配水池", "orange", 
+                 ifelse(Type == "災害用地下給水タンク", "green", "blue")))
 
+shinyServer(function (input, output) {
+  
   # filter data
   filter_df <- reactive({
     if (input$supply.type != "すべて") df <- filter(df, Type == input$supply.type)
@@ -22,46 +27,55 @@ shinyServer(function (input, output) {
   # with leaflet
   output$water_leaf <- renderLeaflet({
     df <- filter_df()
-    leaflet() %>%
+    leaflet(df) %>%
       addTiles() %>%
-      addMarkers(lng=df$Longitude, lat=df$Latitude, popup=df$popup)
+      addMarkers(lng = ~Longitude, lat = ~Latitude, popup = ~popup, 
+                 clusterOptions=markerClusterOptions(maxClusterRadius=40))
   })
   
   # with ggvis
   water_tooltip <- function (x) {
     if (is.null(x)) return (NULL)
     row <- filter(df, id == x$id)
-    # paste0(names(row), ":", format(row), collapse="<br />")
-    paste0(row["popup"])
+    row$popup
   }
   vis <- reactive({
     df <- filter_df()
     df %>% 
-      ggvis(x = ~Longitude, y = ~Latitude, key:= ~id) %>% 
-      layer_points() %>% 
+      ggvis(x = ~Longitude, y = ~Latitude, key := ~id) %>% 
+      layer_points(fill := ~color) %>% 
       add_tooltip(water_tooltip, "hover")
   })  
   bind_shiny(vis, "water_vis")
   
   # with ggmap
-  m.ggmap <- get_map(location=c(mean(df$Longitude), mean(df$Latitude)), zoom=11)
   output$water_ggmap <- renderPlot({
     df <- filter_df()
-    ggmap(m.ggmap) + 
-      geom_point(data=df, aes(x=Longitude, y=Latitude), colour="blue")
+    m.ggmap <- get_map(location=c(mean(df$Longitude), mean(df$Latitude)), 
+                       zoom=ifelse(input$supply.ward == "すべて", 11, 13))
+    p <- switch(input$ggmap.source, 
+                "ggmap"  = ggmap(m.ggmap), 
+                "ggplot" = ggplot())
+    p + 
+      geom_point(data=df, aes(x=Longitude, y=Latitude), colour="navy", size=3.5) +
+      xlab("Longitude") + ylab("Latitude") + theme_bw()
+  })
+  output$ggmap_hover_info <- renderText({
+    df <- filter_df() 
+    nearPoints(df, input$ggmap.hover, xvar="Longitude", yvar="Latitude", 
+               maxpoints=1, threshold=10)$popup
   })
 
   # data table  
   output$water_df <- renderDataTable({
-    df <- filter_df()
-    df %>% select(-popup)
+    filter_df() %>% select(-popup, -color)
   }, options=list(pageLength=10))
   
   # count site
   output$water_text <- renderText({
     df <- filter_df()
     ifelse(nrow(df) > 0, 
-           paste0("対象の給水拠点は ", nrow(df), "件あります"), 
+           sprintf("対象の給水拠点は%s件あります", nrow(df)), 
            "対象の給水拠点はありません")
   })
 })
--- ui.R
+++ ui.R
@@ -16,17 +16,21 @@ shinyUI(fluidPage(
                   selected="すべて"), 
       selectInput("supply.ward", 
                   label="対象区を選んでください", 
-                  choices=list("すべて", "鶴見区", "神奈川区", "西区", "中区", "南区", "港南区", "保土ケ谷区", "旭区", 
-                               "磯子区", "金沢区", "港北区", "緑区", "青葉区", "都筑区", "戸塚区", "栄区", "泉区", "瀬谷区"), 
+                  choices=list("すべて", "鶴見区", "神奈川区", "西区", "中区", "南区", "港南区", 
+                               "保土ケ谷区", "旭区", "磯子区", "金沢区", "港北区", "緑区", 
+                               "青葉区", "都筑区", "戸塚区", "栄区", "泉区", "瀬谷区"), 
                   selected="すべて"), 
       fluidRow(column(10, textOutput("water_text"))), 
       br(), 
       br(), 
       p("このサイトは横浜市のオープンデータを用いています", br(), "詳しくは", 
         a("よこはまオープンデータカタログ", 
-          href="http://www.city.yokohama.lg.jp/seisaku/seisaku/opendata/catalog.html")), 
+          href="http://www.city.yokohama.lg.jp/seisaku/seisaku/opendata/catalog.html"), "へ"), 
       br(), 
-      p("Powered by ", a("shiny", href="http://shiny.rstudio.com"))
+      p("Source on ", 
+        a("GitHub;", href="https://github.com/naokifuse/water_supply_sites_yokohama"), 
+        "Powered by ", 
+        a("shiny", href="http://shiny.rstudio.com"))
     ), 
     
     mainPanel(
@@ -39,12 +43,18 @@ shinyUI(fluidPage(
         tabPanel("ggvis", 
                  h4("Render with", a("ggvis", href="https://github.com/rstudio/ggvis")), 
                  fluidRow(column(10, ggvisOutput("water_vis"))), 
-                 p(br(), "TODO: render with google map")
+                 p(br(), "TODO: render with map")
                  ), 
         tabPanel("ggmap",
                  h4("Render with", a("ggmap", href="https://github.com/dkahle/ggmap")), 
-                 fluidRow(column(10, plotOutput("water_ggmap"))),
-                 p(br(), "TODO: implement infomation popup")
+                 fluidRow(column(10,
+                                 radioButtons("ggmap.source", "Options", 
+                                              choices=list("ggmap", "ggplot"), 
+                                              selected="ggmap"), 
+                                 plotOutput("water_ggmap", hover=hoverOpts("ggmap.hover", 100)), 
+                                 wellPanel(h5("Information"), htmlOutput("ggmap_hover_info"))
+                                 )),
+                 p(br(), "WARNING: When it is rendered with ggmap, hover information does not work well")
                  ), 
         tabPanel("data", 
                  h4("Table"), 
